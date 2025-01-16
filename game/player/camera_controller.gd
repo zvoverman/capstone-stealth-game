736c6fcbffd636d84dd2ca5d61a87472b9b85704
Zach Overman <zvoverman@gmail.com>
2025-01-16 13:38:24 -0700

    3rd person camera implemented

---
+++ game/player/camera_controller.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@onready var yaw_node = $CamYaw
+@onready var pitch_node = $CamYaw/CamPitch
+@onready var camera = $CamYaw/CamPitch/SpringArm3D/Camera3D
+
+var yaw : float = 0
+var pitch : float = 0
+
+var yaw_sensitivity : float = 0.07
+var pitch_sensitivity : float = 0.07
+
+var yaw_acceleration : float = 15
+var pitch_acceleration : float = 15
+
+var pitch_max : float = 75
+var pitch_min : float = -55
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		yaw += -event.relative.x * yaw_sensitivity
+		pitch += event.relative.y * pitch_sensitivity
+
+func _physics_process(delta: float) -> void:
+	pitch = clamp(pitch, pitch_min, pitch_max)
+	
+	yaw_node.rotation_degrees.y = lerp(yaw_node.rotation_degrees.y, yaw, yaw_acceleration * delta)
+	pitch_node.rotation_degrees.x = lerp(yaw_node.rotation_degrees.x, pitch, pitch_acceleration * delta)
--- game/player/player.gd
+++ game/player/player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody3D
 
-@onready var head = $Head
-
 @export var MAX_SPEED = 5.0
 @export var MAX_JUMP_VELOCITY = 4.5
 @export var GRAVITY_STRENGTH = 9.8
@@ -12,22 +10,6 @@ var current_gravity = Vector3.ZERO
 var in_air = false
 
 var current_normal = Vector3.DOWN
-
-const MOUSE_SENSITIVITY = 0.1
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
-		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
-		head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
-
-## Restart game on "r"
-#func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("reset"):
-		#get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -46,27 +28,24 @@ func _physics_process(delta: float) -> void:
 		
 	velocity = MAX_SPEED * get_dir()
 	velocity += current_gravity
-
-	var collision
+		
 	
 	move_and_slide()
-	for i in get_slide_collision_count():
-		collision = get_slide_collision(i)
-		print("I collided with ", collision.get_collider().name)
+	
+	var collision = get_last_slide_collision()
 		
-	if collision and not in_air:
+	if collision:
+		print("I collided with ", collision.get_collider().name, " with normal ", collision.get_normal())
 		self.gravity_direction = -collision.get_normal()
 		self.velocity = velocity.slide(collision.get_normal())
 		current_normal = collision.get_normal()
 	else:
 		self.gravity_direction = Vector3.DOWN
 		current_normal = Vector3.UP
-		
-	print(self.velocity)
 	
 func get_dir() -> Vector3:
 	var dir : Vector3 = Vector3.ZERO
-	var fowardDir : Vector3 = ( $Head/ForwardMarker.global_transform.origin - $Head.global_transform.origin  ).normalized()
+	var fowardDir : Vector3 = ( $CameraRootNode/CamYaw/ForwardMarker.global_transform.origin - global_transform.origin  ).normalized()
 	var dirBase : Vector3= current_normal.cross( fowardDir ).normalized()
 	if Input.is_action_pressed("forward"):
 		dir = dirBase.rotated( current_normal.normalized(), -PI/2 )
