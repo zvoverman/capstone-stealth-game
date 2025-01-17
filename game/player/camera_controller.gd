292b5398098c708c89bfb8285e9cfd40e1050767
Zach Overman <zvoverman@gmail.com>
2025-01-17 12:19:54 -0700

    better movement

--- game/player/camera_controller.gd
+++ game/player/camera_controller.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+class_name CameraController
+
 @onready var yaw_node = $CamYaw
 @onready var pitch_node = $CamYaw/CamPitch
 @onready var camera = $CamYaw/CamPitch/SpringArm3D/Camera3D
@@ -10,11 +12,11 @@ var pitch : float = 0
 var yaw_sensitivity : float = 0.07
 var pitch_sensitivity : float = 0.07
 
-var yaw_acceleration : float = 15
-var pitch_acceleration : float = 15
+var yaw_acceleration : float = 20
+var pitch_acceleration : float = 20
 
-var pitch_max : float = 75
-var pitch_min : float = -55
+var pitch_max : float = 150
+var pitch_min : float = -150
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -22,7 +24,7 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		yaw += -event.relative.x * yaw_sensitivity
-		pitch += event.relative.y * pitch_sensitivity
+		pitch += -event.relative.y * pitch_sensitivity
 
 func _physics_process(delta: float) -> void:
 	pitch = clamp(pitch, pitch_min, pitch_max)
--- game/player/movement_controller.gd
+++ game/player/movement_controller.gd
@@ -1,13 +1,15 @@
 extends CharacterBody3D
 
+class_name MovementController
+
+@onready var forward_direction = $CameraRootNode/CamYaw/CamPitch/ForwardMarker
+
 @export var MAX_SPEED = 5.0
 @export var MAX_JUMP_VELOCITY = 4.5
 @export var GRAVITY_STRENGTH = 9.8
 
 var gravity_direction = Vector3.DOWN
 var current_gravity = Vector3.ZERO
-
-var in_air = false
 
 var current_normal = Vector3.DOWN
 
@@ -17,9 +19,9 @@ func _physics_process(delta: float) -> void:
 		current_gravity += gravity_direction * GRAVITY_STRENGTH * delta
 	else:
 		current_gravity = Vector3.ZERO
+		gravity_direction = Vector3.DOWN
 
 	if Input.is_action_just_pressed("jump"):
-		in_air = true
 		var jump_direction := (transform.basis * Vector3.FORWARD).normalized()
 		current_normal = Vector3.UP
 		velocity.y += MAX_JUMP_VELOCITY
@@ -33,9 +35,17 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	var collision = get_last_slide_collision()
+	
+	var collision_count = get_slide_collision_count()
+	
+	for i in collision_count:
+		var collision_ = get_slide_collision(i)
+		var collider = collision.get_collider().name
+		if collider != 'Floor':
+			print(collider)
 		
 	if collision:
-		print("I collided with ", collision.get_collider().name, " with normal ", collision.get_normal())
+		#print("I collided with ", collision.get_collider().name, " with normal ", collision.get_normal())
 		self.gravity_direction = -collision.get_normal()
 		self.velocity = velocity.slide(collision.get_normal())
 		current_normal = collision.get_normal()
@@ -45,7 +55,7 @@ func _physics_process(delta: float) -> void:
 	
 func get_dir() -> Vector3:
 	var dir : Vector3 = Vector3.ZERO
-	var fowardDir : Vector3 = ( $CameraRootNode/CamYaw/ForwardMarker.global_transform.origin - global_transform.origin  ).normalized()
+	var fowardDir : Vector3 = ( forward_direction.global_transform.origin - global_transform.origin  ).normalized()
 	var dirBase : Vector3= current_normal.cross( fowardDir ).normalized()
 	if Input.is_action_pressed("forward"):
 		dir = dirBase.rotated( current_normal.normalized(), -PI/2 )
