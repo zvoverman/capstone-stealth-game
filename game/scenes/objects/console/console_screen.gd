51c9c872e803a0ab0ccf0a96479066a5a3e88441
Ethan Baedke <ethanbaedke@gmail.com>
2025-04-10 14:11:28 -0600

    Console screen

---
+++ game/scenes/objects/console/console_screen.gd
@@ -0,0 +1,35 @@
+extends Node3D
+
+@export var console_camera : Camera3D
+@export var ui_subviewport : SubViewport
+
+func _input(event: InputEvent) -> void:
+	
+	if event is InputEventMouseMotion or event is InputEventMouseButton:
+		
+		var from : Vector3 = console_camera.project_ray_origin(event.position)
+		var to : Vector3 = from + console_camera.project_ray_normal(event.position) * 1000
+		
+		var hit : Dictionary = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to, 1, []))
+		
+		# Nothing was hit
+		if not hit:
+			return
+			
+		# Something was hit, ensure it was the console screen
+		if not hit["collider"].get_parent() == self:
+			return
+			
+		# Convert world coordinates to uv coordinates for the console screen
+		var shape : CollisionShape3D = hit["collider"].find_child("CollisionShape3D")
+		var position2D : Vector2 = Vector2(shape.global_position.x, shape.global_position.y)
+		var size2D : Vector2 = Vector2(shape.shape.size.x, shape.shape.size.y)
+		var bl : Vector2 = position2D - (size2D / 2.0);
+		var dist : Vector2 = Vector2(hit["position"].x, hit["position"].y) - bl;
+		var uv : Vector2 = dist / size2D;
+		
+		# Convert uv coordinates to screen space coordinates for the console ui
+		event.position = Vector2(uv.x * ui_subviewport.size.x, ui_subviewport.size.y - (uv.y * ui_subviewport.size.y))
+		
+		# Send screen space input to the ui displayed on the console screen
+		ui_subviewport.push_input(event, true)
---
+++ game/ui/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	print("Start button pressed!")
+
+func _on_settings_button_pressed() -> void:
+	print("Settings button pressed!")
+
+func _on_quit_button_pressed() -> void:
+	print("Quit button pressed!")
