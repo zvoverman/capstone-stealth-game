7c87a17f652057fd9c4715b8ba646bb5b60dd0b3
Zach Overman <zvoverman@gmail.com>
2025-04-14 22:39:03 -0600

    dialogue manager + npc as interactable implemented

---
+++ game/dialogue/dialogue_sequence.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name DialogueSequence
+
+@export var lines: Array[String] = []
--- game/scenes/objects/hazards/horizontal_moving_lazer/horizontal_moving_lazer.gd
+++ game/scenes/objects/hazards/horizontal_moving_lazer/horizontal_moving_lazer.gd
@@ -3,7 +3,7 @@
 extends Node3D
 
 @export var distance_from_origin : float = 5.0
-@export var speed : float = 2.0  # Adjust the speed of movement
+@export var speed : float = 2.0
 @export var height : int = 16
 
 var initial_position : float
@@ -11,20 +11,17 @@ var direction : int = 1  # 1 for right, -1 for left
 
 @onready var mesh : CSGCylinder3D = $CSGCylinder3D6
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	initial_position = mesh.position.x  # Save the initial local position (x coordinate) when the node is ready.
+	initial_position = mesh.position.x 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	mesh.height = height
-	# Update the local position (x coordinate) of the laser
 	mesh.position.x += speed * direction * delta
 	
 	# Check if the laser has moved beyond the allowed range in local space
 	if mesh.position.x > initial_position + distance_from_origin:
 		mesh.position.x = initial_position + distance_from_origin
-		direction = -1  # Change direction to left
+		direction = -1
 	elif mesh.position.x < initial_position:
 		mesh.position.x = initial_position
-		direction = 1  # Change direction to right
+		direction = 1
