25bd87fcb713be99cc4e836ff013a45f17f20558
zvoverman <zvoverman@gmail.com>
2025-05-14 18:17:43 -0600

    tooltips appear - area3d issue

--- game/tooltip/tooltip.gd
+++ game/tooltip/tooltip.gd
@@ -1,17 +1,17 @@
-extends Marker3D
+extends Node3D
 
-class_name Checkpoint
+class_name Tooltip
 
-@export var spawn_name : String
+@export var message : String
 
 @onready var collision_area : Area3D = $Area3D
+@onready var label: Label = $Label
 
 signal checkpoint_entered(node: Node3D)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	collision_area.connect("body_entered", _on_body_entered)
-	checkpoint_entered.connect(GameManager._on_checkpoint_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -20,4 +20,6 @@ func _process(_delta: float) -> void:
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "PlayerDrone":
-		checkpoint_entered.emit(self)
+		label.text = message
+		await get_tree().create_timer(1.0).timeout
+		label.visible_ratio = 0.0
